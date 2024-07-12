9e2f3c70718b4978f5954474f42af021b804e253
Minkyoung00 <kina12345600@gmail.com>
2024-07-12 21:37:48 +0900

    Josepus problem file modi

--- WEEK 1/[1866] 요세푸스 문제 0.py
+++ WEEK 1/[1866] 요세푸스 문제 0.py
@@ -1,17 +1,16 @@
-def yoseipus(arr):
-    third = K-1
+def yoseipus(arr,del_idx):
+    del_ord = del_idx-1
     new = []
-
     while True:
-        new.append(arr[third])
-        del arr[third]
+        new.append(arr[del_ord])
+        del arr[del_ord]
 
         if len(arr) == 0:
             return new
-        third = (third +K-1) % len(arr)
+        del_ord = (del_ord +K-1) % len(arr)
 
 N, K = (int(i) for i in input().split())    # 7 3
 people = [ i for i in range(1, N + 1)]  #[1,2,3,4,5,6,7]
 print("<", end="")
-print(*yoseipus(people), sep=", ", end="")
+print(*yoseipus(people,K), sep=", ", end="")
 print(">")
