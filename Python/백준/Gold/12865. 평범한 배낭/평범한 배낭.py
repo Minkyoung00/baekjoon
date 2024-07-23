d3913d4f98529b87f8b61bf6b11bd1c08f8a667b
Minkyoung00 <kina12345600@gmail.com>
2024-07-24 00:08:29 +0900

    [Gold V] Title: 평범한 배낭, Time: 2268 ms, Memory: 34436 KB -BaekjoonHub

--- Python/백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ Python/백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,15 +1,20 @@
-N, K = (int(i) for i in input().split())  # N: 물품 수, K: 최대 무게
+N, K = ( int(i) for i in input().split())   # N:  물품 수, K: 최대 무게 
+
 items = []
-for i in range(N):
-    W, V = (int(i) for i in input().split())  # W: 무게, V: 가치
-    items.append((W, V))
+for i in range(N): 
+    W, V = ( int(i) for i in input().split())   # W: 무게, V: 가치
+    items.append((W,V)) 
+items.sort()
 
-memo = [0] * (K + 1)  
+memo = [0]*(K+1)
 
-for W, V in items:
-    for i in range(K, W - 1, -1):
-        memo[i] = max(memo[i], memo[i - W] + V)
+for W,V in items:
+    new = memo[:]
+    for i in range(K+1):
+        if i + W > K:
+            break      
+        if i == 0 or memo[i] != 0:
+            new[i+W] = max( new[i+W], memo[i] + V)   
+    memo = new
 
-max_val = max(memo)
-
-print(max_val)
+print(max(memo))
