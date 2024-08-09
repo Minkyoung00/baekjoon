937e297d5fdf8d1a4500e0a7d50a7c15f11e10ab
Minkyoung00 <kina12345600@gmail.com>
2024-08-09 10:55:27 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Bronze/2588. 곱셈/곱셈.py
+++ Python/백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,3 +1,8 @@
-A,B = (int(input()),int(input()))
-B1,B2,B3 = (B//100,B%100//10,B%10)
-print(B3*A,B2*A,B1*A,A*B,sep='\n')
+A, B = int(input()), int(input())
+
+B1, B2, B3 = B//100, B%100//10, B%10
+
+print(A * B3)
+print(A * B2)
+print(A * B1)
+print(A * B)
