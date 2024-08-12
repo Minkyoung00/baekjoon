dc716ef9e61a91b3af1cda4c8c151ba49073b37e
Minkyoung00 <kina12345600@gmail.com>
2024-08-12 10:42:40 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- C99/백준/Bronze/2753. 윤년/윤년.c
+++ C99/백준/Bronze/2753. 윤년/윤년.c
@@ -5,8 +5,8 @@ int main(){
     scanf("%d", &year);
 
     if (year % 4 == 0 && (year % 100 != 0 ||year % 400 == 0)) 
-        printf("%d", 1);
-    else printf("%d", 0);
+        printf("1");
+    else printf("0");
     
     return 0;
 }
