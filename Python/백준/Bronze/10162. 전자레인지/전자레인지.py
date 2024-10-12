f34cde2319d9d1034167b72ba79b31266159f5b4
Minkyoung00 <kina12345600@gmail.com>
2024-10-12 13:37:19 +0900

    [Bronze III] Title: 전자레인지, Time: 40 ms, Memory: 31252 KB, Score: 100 point -BaekjoonHub

--- Python/백준/Bronze/10162. 전자레인지/전자레인지.py
+++ Python/백준/Bronze/10162. 전자레인지/전자레인지.py
@@ -1,26 +1,15 @@
 N = int(input())
 result = []
-cnt_min = 1001
 
-A = N // 300
+for i in [300, 60]:
+    if N > 0:
+        result.append(N//i)
+        N %= i
+    else:
+        result.append(0)
 
-for i in range(A+1):
-    temp = []
-    N -= 300*i
-    B = N // 60
-    temp.append(i)
-
-    for j in range(B+1):
-        temp1 = temp[:]
-        N -= 60*j
-        C = N // 10
-        temp1.append(j)
-
-        if N % 10 == 0:
-            temp1.append(C)
-            if sum(temp1) < cnt_min:
-                result = temp1 
-if result:     
-    print(*result)
-else:
+if N % 10:
     print(-1)
+else:
+    result.append(N//10)
+    print(*result)
