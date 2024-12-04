759bd4060f34bfef2878b32a21b67663e78baa5c
Minkyoung00 <kina12345600@gmail.com>
2024-12-04 20:46:52 +0900

    [Gold IV] Title: 이모티콘, Time: 124 ms, Memory: 41828 KB -BaekjoonHub

--- Python/백준/Gold/14226. 이모티콘/이모티콘.py
+++ Python/백준/Gold/14226. 이모티콘/이모티콘.py
@@ -2,49 +2,37 @@ from collections import deque
 
 S = int(input())
 
-result = []
+result = 1000
 
-queue = deque([(1, 1, 1, 0)]) # count, unit, sum, pre(0 :copy, 1 :paste, 2 :delete)
+queue = deque([(1, 1, 1, 0)]) # count(time), unit, sum, pre(0 :copy, 1 :paste, 2 :delete)
 visited = set()
 
 while queue:
     count, unit, sum, pre = queue.popleft()
-    count += 1
-    
-    if (count, unit, sum, pre) in visited:
+
+    if (unit, sum, pre) in visited: continue
+    visited.add((unit, sum, pre))
+
+    if sum == S:
+        result = min(result, count)
         continue
-    visited.add((count, unit, sum, pre))
+    
+    count += 1
 
     if sum > S: continue
 
     # 이전 copy
     elif pre == 0:  
-        if sum + unit == S:
-            result.append(count)
-        else:
-            if not result or (result and min(result) > count):
-                queue.append((count, unit, sum + unit, 1))      # paste
+        if result == 1000 or result > count:
+            queue.append((count, unit, sum + unit, 1))      # paste
+            queue.append((count, unit, sum - 1, 2))         # delete
 
-        if sum - 1 == S:
-            result.append(count)
-        else:
-            if not result or (result and min(result) > count):  # delete
-                queue.append((count, unit, sum - 1, 2))
-
-    # 이전 paste
+    # 이전 paste OR delete
     else:           
-        queue.append((count, sum, sum, 0))  # copy
+        queue.append((count, sum, sum, 0))                  # copy
 
-        if sum + unit == S:                 # paste
-            result.append(count)
-        else:
-            if not result or (result and min(result) > count):
-                queue.append((count, unit, sum + unit, 1)) 
+        if result == 1000 or result > count: 
+            queue.append((count, unit, sum + unit, 1))      # paste
+            queue.append((count, unit, sum - 1, 2))         # delete
 
-        if sum - 1 == S:
-            result.append(count)
-        else:
-            if not result or (result and min(result) > count):  # delete
-                queue.append((count, unit, sum - 1, 2))
-
-print(min(result))
+print(result)
