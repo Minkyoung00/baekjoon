8096fc2594b8b3b2c4760ecfc63490e6ea6f2053
Minkyoung00 <kina12345600@gmail.com>
2025-01-08 10:49:36 +0900

    [Silver III] Title: 통계학, Time: 336 ms, Memory: 87776 KB -BaekjoonHub

--- Python/백준/Silver/2108. 통계학/통계학.py
+++ Python/백준/Silver/2108. 통계학/통계학.py
@@ -1,21 +1,28 @@
 import sys
 from collections import Counter
 
-input = sys.stdin.read
-data = input().split()
-N = int(data[0])
-nums = list(map(int, data[1:]))
+input = sys.stdin.read().split()
+
+N = int(input[0])
+nums = list(map(int, input[1:]))
 
 nums.sort()
 
 cnt_nums = Counter(nums)
 most_common = cnt_nums.most_common()
-max_freq = most_common[0][1]
+freq_n = []
 
-freq_n = sorted(k for k, v in most_common if v == max_freq)
-freq_n = freq_n[1] if len(freq_n) > 1 else freq_n[0]
+for k,v in most_common:
+    if v == most_common[0][1]:
+        freq_n.append(k) 
+
+if len(freq_n) > 1:
+    freq_n.sort()
+    freq_n = freq_n[1]
+else:
+    freq_n = freq_n[0]
 
 print(round(sum(nums) / N)) 
 print(nums[N // 2])         
 print(freq_n)               
-print(nums[-1] - nums[0])    
+print(nums[-1] - nums[0])
